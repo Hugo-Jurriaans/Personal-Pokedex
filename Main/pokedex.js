6bad9b72632cfcac84c8fc395e85508b35c22a8d
Hugo-Jurriaans <jurriaans.hugo@gmail.com>
2026-09-08 22:27:59 +0200

    Added Suggestions to Input

--- Main/pokedex.js
+++ Main/pokedex.js
@@ -7,15 +7,45 @@ async function getPokemonData() {
 }
 
 async function getsuggestions() {
+    let suggestmax = 0;
     const suggestscontainer = document.getElementById("suggestions");
-    for (let i = 0; i <= 3; i++) {
-        const suggest = document.createElement("div");
-        const suggestimage = document.createElement("img");
-        const suggestname = document.createElement("p");
-        const suggestdexnumber = document.createElement("p");
-        suggest.id = `suggestion${i}`;
-        suggestscontainer.appendChild(suggest);
+    const pokedata = JSON.parse(localStorage.getItem("globalpokedata"));
+
+    suggestscontainer.innerHTML = "";
+
+    for (let i = 0; i < 1025; i++) {
+        if (pokedata.results[i].name.startsWith(document.getElementById("searchInput").value.toLowerCase()) && document.getElementById("searchInput").value.length > 0) {
+            if (suggestmax >= 3) break;
+
+            const speciesdata = await fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`);
+            const speciesjson = await speciesdata.json();
+
+            suggestmax++;
+            const suggest = document.createElement("div");
+            const suggestimage = document.createElement("img");
+            const suggestname = document.createElement("p");
+            const suggestdexnumber = document.createElement("p");
+
+            suggest.id = `divsuggestion${i}`;
+            suggestimage.id = `suggestionimage${i}`;
+            suggestimage.src = speciesjson.sprites.other["official-artwork"].front_default;
+            
+            suggestname.id = `suggestionname${i}`;
+            suggestname.textContent = pokedata.results[i].name.charAt(0).toUpperCase() + pokedata.results[i].name.slice(1);
+            suggestdexnumber.id = `suggestiondexnumber${i}`;
+            suggestdexnumber.textContent = `#${i + 1}`;
+
+            suggestscontainer.appendChild(suggest);
+            suggest.appendChild(suggestimage);
+            suggest.appendChild(suggestname);
+            suggest.appendChild(suggestdexnumber);
+
+            console.log(speciesjson);
+        }
     }
+
+
 }
 
+document.getElementById("searchInput").addEventListener("input", getsuggestions);
 getPokemonData();
